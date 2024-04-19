3fcf003fb9bc11d427229500cc256c466cc27492
Nadezhda Ilina <nadiailina9@gmail.com>
2024-04-19 08:36:57 +0300

    сделала лист со случайным фильмом

--- src/pages/RandomFilm.tsx
+++ src/pages/RandomFilm.tsx
@@ -1,7 +1,104 @@
 import React from 'react';
+import { Button, Card } from 'antd';
+import Meta from 'antd/es/card/Meta';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { apiKey } from '../core/redux/slices/apiSlice';
+import { Movies } from '../@types/types';
+import defaultposter from '../assets/image/default.svg';
+import '../assets/styles/RandomMovie.module.scss';
 
-const RandomFilm = () => {
-  return <div>RandomFilm</div>;
+const RandomFilm: React.FC = () => {
+  const [item, setItem] = React.useState<Movies>();
+  const min = 666;
+  const max = 2000;
+
+  const id = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  const getRandomMovie = async () => {
+    const newId = Math.floor(Math.random() * (max - min + 1)) + min;
+    try {
+      const { data } = await axios.get(`https://api.kinopoisk.dev/v1.4/movie/${newId}`, {
+        headers: {
+          accept: 'application/json',
+          'X-API-KEY': apiKey,
+        },
+      });
+      setItem(data);
+    } catch (error) {
+      console.error('Ошибка при получении нового фильма:', error);
+    }
+  };
+
+  React.useEffect(() => {
+    async function fetchMovie() {
+      try {
+        const { data } = await axios.get(`https://api.kinopoisk.dev/v1.4/movie/${id}`, {
+          headers: {
+            accept: 'application/json',
+            'X-API-KEY': apiKey,
+          },
+        });
+        setItem(data);
+      } catch (error) {
+        console.error('Ошибка при получении фильма:', error);
+      }
+    }
+
+    fetchMovie();
+  }, []);
+
+  if (!item) {
+    return <>Загрузка...</>;
+  }
+
+  const formatCountries = (countries: { name: string }[]): string => {
+    return countries.map((country) => country.name).join(', ');
+  };
+
+  const getFirstTwoGenres = (genres: { name: string }[]): string => {
+    if (genres.length >= 2) {
+      return `${genres[0].name}, ${genres[1].name}`;
+    } else if (genres.length === 1) {
+      return genres[0].name;
+    } else {
+      return '';
+    }
+  };
+
+  return (
+    <div className='random-movie'>
+      <h1>Выбрать случайный фильм </h1>
+      <Link to={`/movie/${item.id}`}>
+        <Card
+          className='random-movie__card'
+          hoverable
+          style={{ width: 250 }}
+          cover={
+            <img
+              alt='film'
+              src={
+                item.poster && item.poster.url && item.poster.url !== 'null'
+                  ? item.poster.url
+                  : defaultposter
+              }
+            />
+          }
+        >
+          <Meta title={item.name ? item.name : item.alternativeName} description='' />
+          <div>
+            <p>
+              {formatCountries(item.countries)} •{' '}
+              {item.genres ? getFirstTwoGenres(item.genres) : ''}
+            </p>
+            <p>Год: {item.year}</p>
+            <p>Возрастной рейтинг: {item.ageRating ? item.ageRating : 'Нет'}</p>
+          </div>
+        </Card>
+      </Link>
+      <Button onClick={getRandomMovie}>Получить фильм</Button>
+    </div>
+  );
 };
 
 export default RandomFilm;
